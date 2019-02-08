8c16cadaef9aec8b256fa1a934f1526ebb7ff16a
Ed Hollingsworth <ehollingsworth@hq.prizum.com>
2019-02-08 18:02:03 -0500

    Updated to work with PostgreSQL database; updated ConfigControllerTests to JUnit 5

--- src/main/java/com/ionep/egis/services/config/repository/CityRepository.java
+++ src/main/java/com/ionep/egis/services/config/repository/CityRepository.java
@@ -3,9 +3,11 @@ package com.ionep.egis.services.config.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ionep.egis.services.config.City;
 
+@Repository
 public interface CityRepository extends CrudRepository<City, Long> {
 	
 	Optional<City> findByName(String name);
